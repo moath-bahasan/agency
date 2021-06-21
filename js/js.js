d1ff2b09a81f5ecb10ee7d4ecec5cc11def5efe3
moath bahasan <moazbahasan@gmail.com>
2021-06-21 06:33:52 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+// لتغير خصائص القائمة عند النزول إلى الأسفل noTransparrent إضافة الخاصية 
+$(window).scroll(function(){
+    if($(this).scrollTop()>=500){
+       $("#navBar").addClass("noTransparrent");
+     }else{
+       $("#navBar").removeClass("noTransparrent");
+     }
+});
+// تحرك الصفحة بين عناصر القائمة ضمن فترة زمنية محددة
+$(document).ready(function(){
+    $("a.scroll").on('click', function(event) {
+ 
+         var hash = this.hash;
+ 
+         $('html, body').animate({scrollTop: $(hash).offset().top - 100}, 800, function(){});
+     
+     });
+
+     // countTo تطبيق التابع
+    $('.timer').countTo();
+
+// التحقق من إدخال البيانات الصحيحة في نموذج الإرسال
+$(document).ready(function() {
+  $(function () {
+      $("#commentForm").validate();
+  });
+});
+
+});
